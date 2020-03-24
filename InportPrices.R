39d903bd804bacb4ea1b4ef86e1736e536b5506c
ELsamar <33390307+ELsamar@users.noreply.github.com>
2020-03-24 04:18:07 +0100

    Delete InportPrices.R

--- InportPrices.R
+++
@@ -1,7 +0,0 @@
-# the procedure for importing historical stock prices into R.
-install.packages("quantmod")
-library(quantmod)
-getSymbols("PG",from="2020-01-02",to="2020-01-31",auto.assign = TRUE)
-close<-PG$PG.Close
-mean(close)
-ts.plot(close)
